6f069be0f8e5d0253696b7a9893711e37ba62ad4
GitHub Action <action@github.com>
2021-09-13 13:20:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/adamathsbc/intellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.adamathsbc.intellijplatformplugin.services
+
+import com.github.adamathsbc.intellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
